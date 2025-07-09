33611a640742257209a6966a7765ba1fc444bb69
Basma-Almutlaq <basmaalmutlaq04@gmail.com>
2025-07-09 11:49:47 +0300

    Delete ConsoleApp2 directory

--- ConsoleApp2/ConsoleApp2/Models/ Instructor.cs
+++
@@ -1,7 +0,0 @@
-public class Instructor
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Email { get; set; }
-    public List<CourseInstructor>? CourseInstructors { get; set; }
-}
--- ConsoleApp2/ConsoleApp2/Models/CourseInstructor.cs
+++
@@ -1,7 +0,0 @@
-public class CourseInstructor
-{
-    public int CourseId { get; set; }
-    public Course? Course { get; set; }
-    public int InstructorId { get; set; }
-    public Instructor? Instructor { get; set;}
-}
