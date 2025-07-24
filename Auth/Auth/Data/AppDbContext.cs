561e6f85ec65474859e6ccbea004277730033738
Basma-Almutlaq <basmaalmutlaq04@gmail.com>
2025-07-24 16:01:29 +0300

    Delete Auth directory

--- Auth/Auth/Data/AppDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-public class AppDbContext : IdentityDbContext<IdentityUser>
-{
-public AppDbContext(DbContextOptions options) : base(options) {}
-public DbSet<Product> Products { get; set;}
-}
--- Auth/Auth/Options/JwtSettings.cs
+++
@@ -1,6 +0,0 @@
-public class JwtSettings{
-
-public string? SigningKey { get; set; }
-public string? Issuer { get; set; }
-public string[]? Audiences { get; set; }
-}
